29e578c0ee88e8d76a0e47a246967e36ec835798
LenZuro <126126939+LenZuro@users.noreply.github.com>
2023-06-19 20:19:54 -0700

    more JS stuff being added

--- develop/pub/assets/js/index.js
+++ develop/pub/assets/js/index.js
@@ -11,3 +11,37 @@ if (window.location.pathname === "/notes") {
     newNoteBtn = document.querySelector(".new-note");
     noteList = document.querySelector(".list-container .listgroup")
 }
+
+const show = (elem) => {
+    elem.style.display = "inline";
+};
+
+const hide = (elem) => {
+    elem.style.display = "none";
+};
+
+let activeNotes = {};
+
+const getNote = () => 
+    fetch("/api/notes" , {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+const saveNote = (note) =>
+    fetch("/api/notes" , {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json",
+        },
+    });
+
+const deleteNote =  (id) =>
+    fetch(`/api/notes/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "Application/json",
+        },
+    });
